fd855407caa7d8aab6f5bfe84ec9ffefa5f085f1
Gayle Martin <hello.gmartin@gmail.com>
2024-02-26 13:20:34 -0600

    Complete website

---
+++ website/static/index.js
@@ -0,0 +1,8 @@
+function deleteNote(noteId) {
+  fetch('/delete-note', {
+    method: 'POST',
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((res) => {
+    window.location.href = '/'
+  })
+}
